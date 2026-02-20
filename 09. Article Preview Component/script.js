78e97edc4d86255732f9b5e9b03736a0be7442b2
Sayani Mondal (Wanderer) <sayanimondal604@gmail.com>
2026-02-20 12:06:29 +0530

    Refactor CSS for Article Preview Component

--- 09. Article Preview Component/script.js
+++ 09. Article Preview Component/script.js
@@ -1,12 +1,20 @@
 // DOM references
-const shareButtonEl = document.querySelector(".share-button");
+const shareButtonEl = document.querySelector("#share-button");
 const socialLinksToolTipEl = document.querySelector(
-  ".social-links-tooltip-invisible",
+  "#social-links-tooltip-invisible",
 );
+const statusAnnounce = document.querySelector("#share-status");
 
 function shareLinksFunction() {
   socialLinksToolTipEl.classList.toggle("social-links-tooltip-invisible");
   socialLinksToolTipEl.classList.toggle("social-links-tooltip-visible");
+
+  // Screen reader announcement
+  statusAnnounce.textContent = socialLinksToolTipEl.classList.contains(
+    "social-links-tooltip-invisible",
+  )
+    ? "Share options hidden."
+    : "Share options shown.";
 }
 
 // Event listener
